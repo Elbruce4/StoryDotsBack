886274b52011984fbc7fc3b51e7c8c4ff646b322
Elbruce4 <78322819+Elbruce4@users.noreply.github.com>
2023-05-03 15:46:05 -0300

    update repo

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import express from "express"
 const app = express();
-const port = 5000;
 import {sequelize} from "./db.js"
 import "./models/Products.js"
 import proyectRoutes from "./routes/routes.js"
+import * as dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(proyectRoutes);
 
-/* app.get('/', function (req, res) {
-    console.log("volvemos a la home");
-    res.send('GET request to homepages')
-})
+console.log(port);
 
-app.get('/products', function (req, res) {
-    var products = fetch('https://fakestoreapi.com/products')
-    .then(res=>res.json())
-    .then(json=>console.log(json))
-    console.log(products);
-})
- */
 async function main () {
     try {
         await sequelize.sync();
